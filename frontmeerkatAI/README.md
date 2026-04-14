881b7195a98bb600484d3a02a89e1dc8624d0be7
yuhandemian <yuhn1011@naver.com>
2026-04-14 12:16:57 +0900

    Refactor: remove old frontend and rename latest frontend folder

--- frontmeerkatAI/README.md
+++
@@ -1,4 +0,0 @@
-# 실행 순서
-
-1. npm install
-2. npm run dev
--- frontmeerkatAI/src/apis/mainApi.js
+++
@@ -1,9 +0,0 @@
-import { api } from "./client";
-
-export const mainApi = {
-  getVideoList: (filter) => api.post("/video/list", filter, true),
-  viewVideo: (id) => api.get(`/video/view/${id}`, {}, true),
-  downloadVideo: (ids) => api.post("/video/download", ids, true, "blob"),
-  deleteVideo: (ids) => api.delete("/video/delete", ids, true),
-  loadCalendar: (date) => api.get(`/calendar/${date}`, {}, true),
-};
